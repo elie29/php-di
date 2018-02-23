3b2ec1db3c7e8e7452c6dd2a1374165a438b7cc0
Elie NEHME <elie29@gmail.com>
2018-02-23 14:45:11 +0100

    Used best approach

--- public/index.php
+++ public/index.php
@@ -6,7 +6,6 @@ use App\Service\Logger\Logger;
 use App\Service\Random\Microtime;
 use App\Service\Random\Random;
 use App\Service\Random\Uniqid;
-use Psr\Container\ContainerInterface;
 
 chdir(dirname(__DIR__));
 require 'vendor/autoload.php';
@@ -14,29 +13,14 @@ require 'vendor/autoload.php';
 // Container creation
 $builder = new DI\ContainerBuilder();
 $builder->addDefinitions([
-    // Logger is created even if it isn't used
-    ILogger::class => new Logger(),
-    // Using the container interface
-    'random' => function (ContainerInterface $c) {
-        return new Random($c->get(ILogger::class));
-    },
-    // Using type-hinting as long as ILogger is registered
-    'random2' => function (ILogger $logger) {
-        return new Random($logger);
-    },
-    // Better approach
-    'uniqid' => function (ContainerInterface $c) {
-        return $c->get(Uniqid::class);
-    },
-    // Best approach
-    'microtime' => DI\get(Microtime::class),
+    ILogger::class => DI\get(Logger::class),
+    'random'       => DI\get(Random::class),
+    'uniqid'       => DI\get(Uniqid::class),
+    'microtime'    => DI\get(Microtime::class),
 ]);
 $container = $builder->build();
 
 $controller = new IndexController($container->get('random'));
-dump($controller->getRandomAction());
-
-$controller = new IndexController($container->get('random2'));
 dump($controller->getRandomAction());
 
 $controller = new IndexController($container->get('uniqid'));
